0fbd78324852359c69e8f426064af42ddfb4d483
Jeff Talbot <jeff.a.talbot@gmail.com>
2012-07-31 17:12:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rover::Application.config.secret_token = '11d5fd0394ad7b05e8f415cddfd3082510dd43019abea49520bd256c499ad4696b3f9f2417ee23762982ed7a3183138d827b5020a0d056a854298eb0f1c81e25'
