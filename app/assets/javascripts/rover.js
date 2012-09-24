d8e25b43542a4af3d266c188283439390918d54a
Jeff Talbot <jeff.a.talbot@gmail.com>
2012-09-24 17:33:12 -0500

    Refactored var declarations

--- app/assets/javascripts/rover.js
+++ app/assets/javascripts/rover.js
@@ -7,61 +7,61 @@ RVR.rover = function(params) {
       instance = {},
       position,
       rotation,
-      cruise;
+      cruise,
+      reset = function() {
+        position = params.position || {x: 0, y: 0};
+        rotation = params.rotation || 0;
+        cruise = false;
+      },
+      getPosition = function() {
+        return position;
+      },
+      getRotation = function() {
+        return rotation;
+      },
+      rotate = function(degrees) {
+        rotation += degrees;
+        if (rotation < 0) {
+          rotation = 270;
+        }
+      },
+      setCruise = function(bool) {
+        cruise = bool;
+      },
+      isMoving = function() {
+        return cruise;
+      },
+      nextPosition = function() {
+        var x = position.x,
+            y = position.y;
 
-  var reset = function() {
-    position = params.position || {x: 0, y: 0};
-    rotation = params.rotation || 0;
-    cruise = false;
-  };
-  instance.reset = reset;
+        if (rotation === 0) {
+          x += 1;
+        } else if (rotation === 90) {
+          y -= 1;
+        } else if (rotation === 180) {
+          x -= 1;
+        } else if (rotation === 270) {
+          y += 1;
+        }
 
-  instance.getPosition = function() {
-    return position;
-  };
-
-  instance.getRotation = function() {
-    return rotation;
-  };
-
-  instance.rotate = function(degrees) {
-    rotation += degrees;
-    if (rotation < 0) {
-      rotation = 270;
-    }
-  };
-
-  instance.setCruise = function(bool) {
-    cruise = bool;
-  };
-
-  instance.isMoving = function() {
-    return cruise;
-  };
-
-  var nextPosition = function() {
-    var x = position.x,
-        y = position.y;
-
-    if (rotation === 0) {
-      x += 1;
-    } else if (rotation === 90) {
-      y -= 1;
-    } else if (rotation === 180) {
-      x -= 1;
-    } else if (rotation === 270) {
-      y += 1;
-    }
-
-    return {x: x, y: y};
-  };
-  instance.nextPosition = nextPosition;
-
-  instance.updatePosition = function() {
-    position = nextPosition();
-  };
+        return {x: x, y: y};
+      },
+      updatePosition = function() {
+        position = nextPosition();
+      };
 
   reset();
+
+  instance.reset = reset;
+  instance.getPosition = getPosition;
+  instance.getRotation = getRotation;
+  instance.rotate = rotate;
+  instance.setCruise = setCruise;
+  instance.isMoving = isMoving;
+  instance.updatePosition = updatePosition;
+  instance.nextPosition = nextPosition;
+
   return instance;
 };
 
