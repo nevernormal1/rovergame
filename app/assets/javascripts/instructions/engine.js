2ab8d2bcff6db5b85aa04a832f5426a6bb459d7a
Jeff Talbot <jeff.a.talbot@gmail.com>
2012-10-29 07:40:35 -0500

    Reorganizing instruction groups

--- app/assets/javascripts/instructions/engine.js
+++ app/assets/javascripts/instructions/engine.js
@@ -3,15 +3,26 @@ if (typeof RVR === 'undefined') {
 }
 
 (function() {
-  var group = RVR.instructionGroups.add({label: "Engine"});
+  var group = RVR.instructionGroups.add({label: "Controls"});
 
   group.addInstruction({
     value: "engine",
-    label: "Engine [option]",
-    selectLabel: "Engine",
+    label: "Turn Engine [On/Off]",
+    selectLabel: "Turn Engine",
     args: [{name: "engine", label: "Turn Engine", values: ["On", "Off"]}]
   }).perform(function(args) {
     this.setCruise(args.engine === "On");
   });
+  group.addInstruction({
+    label: "Rotate [Direction]",
+    selectLabel: "Rotate",
+    value: "rotate",
+    args: [{name: "rotate", label: "Rotate", values: ["Left", "Right"]}]
+  }).perform(function(args) {
+    var degrees = args.rotate === "Left" ? 90 : -90;
+
+    this.rotate(degrees);
+  });
+
 })();
 
--- app/assets/javascripts/instructions/sensor.js
+++ app/assets/javascripts/instructions/sensor.js
@@ -3,7 +3,44 @@ if (typeof RVR === 'undefined') {
 }
 
 (function() {
-  var group = RVR.instructionGroups.add({label: "Test"});
+  var group = RVR.instructionGroups.add({label: "Sensor"});
+
+  group.addInstruction({
+    label: "If Heading Is [Direction]",
+    args: [
+      {name: "condition", label: "If", values: ["Heading"]},
+      {name: "value", label: "Is", values: ["North", "South", "East", "West"]}
+    ],
+    value: "if",
+    block: true
+  }).perform(function(args, instructions, elseInstructions) {
+    var direction = args.value,
+        currDirection = this.direction();
+
+    if (currDirection === direction) {
+      instructions.forEach(function(instruction) {
+        instruction.callback();
+      });
+    } else {
+      elseInstructions.forEach(function(instruction) {
+        instruction.callback();
+      });
+    }
+  });
+
+  group.addInstruction({
+    label: "When Blocked",
+    selectLabel: "When Blocked",
+    value: "navigation.blocked",
+    block: true
+  }).perform(function(args, instructions) {
+    var rover = this;
+    rover.dispatch.on("blocked", function() {
+      instructions.forEach(function(instruction) {
+        instruction.callback();
+      });
+    });
+  });
 
   group.addInstruction({
     label: "If Blocked to the [Direction]",
