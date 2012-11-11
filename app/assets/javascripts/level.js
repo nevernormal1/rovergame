812a701ccce6fd250aa9d1ec4f1b7c87eba5764b
Jeff Talbot <jeff.a.talbot@gmail.com>
2012-11-11 09:11:13 -0600

    Zigzag level pulled out

--- app/assets/javascripts/level.js
+++ app/assets/javascripts/level.js
@@ -2,44 +2,13 @@ if (typeof RVR === 'undefined') {
   RVR = {};
 }
 
-RVR.level = function(spec, my) {
-  var grid,
-      parent,
-      walls,
-      data,
+RVR.level = function(my) {
+  var parent,
       that = {},
 
       collision = function(position) {
-        if (data[position.y][position.x] === 1) return true;
+        if (my.data[position.y][position.x] === 1) return true;
         return false;
-      },
-
-      render = function() {
-        var wallSelection, wallBlocks;
-
-        wallSelection = parent.selectAll(".wall").data(walls);
-        wallSelection.enter()
-          .append("g")
-          .attr("class", "wall");
-
-        wallSelection.exit().remove();
-
-        wallBlocks = wallSelection.selectAll(".block")
-          .data(function(d) { return d; });
-
-        wallBlocks.enter()
-          .append("rect")
-          .attr("class", "block")
-          .attr("x", grid.offsetX(0.5))
-          .attr("y", grid.offsetY(0.5))
-          .attr("rx", "2")
-          .attr("ry", "2")
-          .attr("height", grid.getCellHeight() + 0.5)
-          .attr("width", grid.getCellWidth() + 0.5);
-
-        wallBlocks.transition()
-          .attr("x", grid.offsetX(0.5))
-          .attr("y", grid.offsetY(0.5));
       };
 
   my = my || {};
@@ -55,8 +24,6 @@ RVR.level = function(spec, my) {
   };
 
   that.collision = collision;
-  that.reset = reset;
-  that.render = render;
 
   return that;
 }
--- app/assets/javascripts/levels/zigzag.js
+++ app/assets/javascripts/levels/zigzag.js
@@ -2,10 +2,14 @@ if (typeof RVR === 'undefined') {
   RVR = {};
 }
 
-(function() {
-  var generateWallCount = function() {
+RVR.zigzag = function() {
+  var my = {},
+      that = RVR.level(my),
+      walls,
+
+      generateWallCount = function() {
         var randomWallCount = Math.ceil(Math.random() * 4) * 2,
-            maxWallCount = Math.floor((grid.getRowCount() - 1) / 2);
+            maxWallCount = Math.floor((my.grid.getRowCount() - 1) / 2);
 
         if (randomWallCount > maxWallCount) {
           return maxWallCount;
@@ -14,48 +18,79 @@ if (typeof RVR === 'undefined') {
       },
 
       buildWalls = function() {
-        var i, j, y=0, minY=1, maxY, wall, wallCount = generateWallCount(), walls;
+        var i, j, y=0, minY=1, maxY, wall, wallCount = generateWallCount();
 
         walls = [];
 
         for(i=0; i<wallCount; i++) {
           wall = new Array();
-          maxY = grid.getRowCount() - 1 - ((wallCount - i - 1) * 2)
+          maxY = my.grid.getRowCount() - 1 - ((wallCount - i - 1) * 2)
           y = Math.floor(Math.random() * (maxY - minY)) + minY;
           if (i % 2 === 0) { // Left wall
-            for(j = 0; j< grid.getColumnCount() - 2; j++) {
+            for(j = 0; j< my.grid.getColumnCount() - 2; j++) {
               wall.push({x: j, y: y});
             }
           } else { // Right wall
-            for(j = 2; j < grid.getColumnCount(); j++) {
+            for(j = 2; j < my.grid.getColumnCount(); j++) {
               wall.push({x: j, y: y});
             }
           }
           walls.push(wall);
           minY = y + 2;
         }
-
-        return walls;
       },
 
       populateGrid = function() {
-        var data, i, j, wall, wallPoint, walls = buildWalls();
+        var i, j, wall, wallPoint;
 
-        data = new Array.matrix(grid.getRowCount(), grid.getColumnCount(), 0);
+        buildWalls();
+
+        my.data = new Array.matrix(my.grid.getRowCount(), my.grid.getColumnCount(), 0);
 
         for(i=0; i<walls.length; i++) {
           wall = walls[i];
           for(j=0; j<wall.length; j++) {
             wallPoint = wall[j];
-            data[wallPoint.y][wallPoint.x] = 1;
+            my.data[wallPoint.y][wallPoint.x] = 1;
           }
         }
+      },
+
+      render = function() {
+        var wallSelection, wallBlocks;
+
+        wallSelection = my.parent.selectAll(".wall").data(walls);
+        wallSelection.enter()
+          .append("g")
+          .attr("class", "wall");
+
+        wallSelection.exit().remove();
+
+        wallBlocks = wallSelection.selectAll(".block")
+          .data(function(d) { return d; });
+
+        wallBlocks.enter()
+          .append("rect")
+          .attr("class", "block")
+          .attr("x", my.grid.offsetX(0.5))
+          .attr("y", my.grid.offsetY(0.5))
+          .attr("rx", "2")
+          .attr("ry", "2")
+          .attr("height", my.grid.getCellHeight() + 0.5)
+          .attr("width", my.grid.getCellWidth() + 0.5);
+
+        wallBlocks.transition()
+          .attr("x", my.grid.offsetX(0.5))
+          .attr("y", my.grid.offsetY(0.5));
       };
 
-  return RVR.level().reset(function() {
+  that.reset = function() {
     buildWalls();
     populateGrid();
-  });
+  };
 
-})();
+  that.render = render;
+
+  return that;
+};
 
